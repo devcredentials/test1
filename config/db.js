c9e395c2d55686347a81e40922a51dbdd5a7fedb
Dev Credentials <mydevcredentials@gmail.com>
2016-04-27 21:28:23 +0530

    FC

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
-		'url': 'mongodb://amit:gautam@ec2-54-200-0-89.us-west-2.compute.amazonaws.com:27017/dbtwelo'
+	//	'url': 'mongodb://amit:gautam@ec2-54-200-0-89.us-west-2.compute.amazonaws.com:27017/dbtwelo'//admin
+		'url': 'mongodb://usertwelo:pwdtwelo@ec2-54-200-0-89.us-west-2.compute.amazonaws.com:27017/dbtwelo'
 		//'url': 'mongodb://localhost/dbtwelo'
 //	'url': 'mongodb://dbuser:dbuser@ds045064.mlab.com:45064/dbwaneloc'
 //	mongodb://<dbuser>:<dbpassword>@ds045064.mlab.com:45064/dbwaneloc
