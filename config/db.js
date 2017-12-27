9effe875daad850aa704d528de153f00a842a737
devcredentials <mydevcredentials@gmail.com>
2017-12-27 14:46:33 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,4 @@
 module.exports = {
-	//	'url': 'mongodb://amit:gautam@ec2-54-200-0-89.us-west-2.compute.amazonaws.com:27017/dbtwelo'//admin
-		'url': 'mongodb://usertwelo:pwdtwelo@ec2-54-200-0-89.us-west-2.compute.amazonaws.com:27017/dbtwelo'
-		//'url': 'mongodb://localhost/dbtwelo'
-//	'url': 'mongodb://dbuser:dbuser@ds045064.mlab.com:45064/dbwaneloc'
-//	mongodb://<dbuser>:<dbpassword>@ds045064.mlab.com:45064/dbwaneloc
+
+	mongodb://<dbuser>:<dbpassword>@pwd:45064/dbwanelocdb
 }
